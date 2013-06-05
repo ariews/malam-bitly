d0fa177b5aa0ca9d645242e4f3e773fa62a303b7
Arie W. Subagja <arie@malam.or.id>
2013-06-05 12:43:38 +0700

    3.2 support

--- classes/malam/bitly.php
+++ classes/malam/bitly.php
@@ -10,11 +10,13 @@ class Malam_Bitly
 {
     const API_URL = 'http://api.bit.ly/v3/';
 
-    private $config;
+    protected $config;
+
+    protected $respond;
 
     public function __construct()
     {
-        $this->config = Kohana::config('bitly');
+        $this->config = Kohana::$config->load('bitly');
     }
 
     public static function instance()
@@ -35,8 +37,40 @@ class Malam_Bitly
             'format'    => 'json',
         );
 
-        $resp = Remote::get(Bitly::API_URL.'shorten'.URL::query($params));
+        $respond = Request::factory(Bitly::API_URL.'shorten'.URL::query($params))
+                    ->method(Request::GET)
+                    ->execute();
 
-        return $resp;
+        $this->respond = json_decode($respond->body());
+
+        if ($this->is_error())
+        {
+            throw new Kohana_Exception('Error [:code]: :message', array(
+                ':code'     => $this->error_code(),
+                ':message'  => $this->error_msg()
+            ));
+        }
+
+        return $this;
+    }
+
+    public function is_error()
+    {
+        return $this->respond->status_code != 200;
+    }
+
+    public function error_code()
+    {
+        return $this->respond->status_code;
+    }
+
+    public function error_msg()
+    {
+        return $this->respond->status_txt;
+    }
+
+    public function short()
+    {
+        return $this->respond->data->url;
     }
 }
